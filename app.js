7a459e06235a45901c15fd7763d0b3707a8d6302
Hay1tov <eldorh35@gmail.com>
2025-07-26 19:30:35 +0500

    update

---
+++ app.js
@@ -0,0 +1,91 @@
+
+//                                    No_1
+
+// let a = 2.5;
+// let n = 3;
+
+// let result = 1;
+
+// if (n > 0) {
+//   for (let i = 1; i <= n; i++) {
+//     result *= a;
+//   }
+
+//   console.log(`${a} ning ${n}-darajasi: ${result}`);
+
+// } else {
+
+//   console.log("(n > 0) bo'lishi kerak");
+// }
+
+//                                    No_2
+
+
+// let a = 2.5;
+// let n = 4;
+
+// if (n > 0) {
+//   let result = 1;
+
+//   for (let i = 1; i <= n; i++) {
+//     result *= a;
+//     console.log(`${a} ning ${i}-darajasi: ${result}`);
+//   }
+// } else {
+//   console.log("(n > 0) bo'lishi kerak");
+// }
+
+//                                    No_3
+
+// let a = 2.5;
+// let n = 4;
+
+// let result = 1;
+// let total = 0;
+
+// if (n > 0) {
+
+//   for (let i = 1; i <= n; i++) {
+//     result *= a;
+//     console.log(`${a} ning ${i}-darajasi: ${result}`);
+//     total += result;
+//   }
+
+//   console.log(`Yig'indi: ${total}`);
+// } else {
+//   console.log("(n > 0) bo'lishi kerak");
+// }
+
+
+//                                    No_4
+
+// let n = 5;
+
+// let factorial = 1;
+// let total = 0;
+
+// if (n > 0) {
+
+//   for (let i = 1; i <= n; i++) {
+//     factorial *= i;
+//     total += factorial;
+//     console.log(`${i}! = ${factorial}`);
+//   }
+
+//   console.log(`Yig'indi S = ${total}`);
+// } else {
+//   console.log("(n > 0) bo'lishi kerak");
+// }
+
+//                                    No_5
+
+
+
+
+//                                    No_6
+
+
+
+
+
+//                                    No_7
