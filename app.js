141f12c2e5b91dae272d24079a1fa79bd073f74e
Hay1tov <eldorh35@gmail.com>
2025-07-27 18:08:52 +0500

    update

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//     10 tasini avvalgi darsda uyga vazifa bergandingiz shuning uchun ishlamadim
 
 //                                    No_1
 
@@ -79,13 +80,121 @@
 
 //                                    No_5
 
+// let N = 5;
+// let K = 3;
 
+// let S = 0;
 
+// for (let i = 1; i <= N; i++) {
+//   S += i * K;
+// }
+
+// console.log("S = " + S);
 
 //                                    No_6
 
+// let N = 5;
 
+// let S = 0;
 
+// for (let i = 1; i <= N; i++) {
+//   S += i * i;
+// }
 
+// console.log("S = " + S);
 
 //                                    No_7
+
+// let A = 1;
+// let B = 5;
+
+// for (let i = A; i <= B; i++) {
+//   for (let j = 0; j < i - A + 1; j++) {
+//     console.log(i);
+//   }
+// }
+
+
+//                                    No_8
+
+// let number = 36;
+
+// let total = 0;
+// let loop = 0;
+
+// for (let i = 1; i <= number; i++) {
+//   if (number % i == 0) {
+//     loop++;
+//     total += i
+//   }
+// }
+
+// console.log(`${number} ning ${loop} ta bo'linuvchisi bor`)
+// console.log(`${number} ning bo'linuvchilar yig'indi ${total}`)
+
+//                                    No_9
+
+// let N = 103;
+
+// let bolinuvchi = 0;
+
+// if (N > 1) {
+//   for (let i = 1; i <= N; i++) {
+//     if (N % i == 0) {
+//       bolinuvchi++;
+//     }
+//   }
+//   if (bolinuvchi == 2) {
+//     console.log("N tub son");
+//   } else {
+//     console.log("N tub son emas");
+//   }
+
+// } else {
+
+//   console.log("N > 1 bo'lishi kerak")
+// }
+
+//                                    No_10
+
+// let number = 10;
+
+// for (let i = 1; i <= number; i++) {
+//   let qator = "";
+//   for (let j = 1; j <= i; j++) {
+//     qator += j + " ";
+//   }
+//   console.log(qator);
+// }
+
+
+//                                   W H I L E
+//                                    No_11
+
+// let A = 29;
+// let B = 5;
+
+// let qoldiq = A;
+
+// while (qoldiq >= B) {
+//   qoldiq = qoldiq - B;
+// }
+
+// console.log("Bo'sh qolgan kesma uzunligi:", qoldiq);
+
+//                                    No_11
+
+
+//                                    No_11
+
+
+//                                    No_11
+
+
+//                                    No_11
+
+
+//                                    No_11
+
+
+//                                    No_11
